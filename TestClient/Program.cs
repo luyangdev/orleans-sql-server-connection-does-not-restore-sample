1387891946462f1f9172a1e41045977e331ae7cf
luyang <luyang.net@outlook.com>
2021-07-08 11:58:56 +0300

    add connection retry setting to db connection string

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -21,12 +21,14 @@ namespace TestClient
             Console.WriteLine("\n== Step 1. Should OK ==");
             Console.WriteLine("Make sure sql server is running now, and press any key to continue");
             Console.ReadKey();
+            Console.WriteLine("Key pressed");
             var result = await client.GetGrain<ITaskGrain>("key").Execute();
             Console.WriteLine(result.Count);
 
             Console.WriteLine("\n== Step 2. Should Exception ==");
             Console.WriteLine("Stop sql server now, and press any key to continue");
             Console.ReadKey();
+            Console.WriteLine("Key pressed");
 
             try
             {
@@ -38,24 +40,13 @@ namespace TestClient
                 Console.WriteLine(ex.ToString());
             }
 
-            Console.WriteLine("\n== Step 3. Should OK, try several times ==");
+            Console.WriteLine("\n== Step 3. Should OK ==");
             Console.WriteLine("Restart sql server now, and press any key to continue");
             Console.ReadKey();
+            Console.WriteLine("Key pressed");
 
-            for (var i = 0; i < 5; i++)
-            {
-                Console.WriteLine($"-- Step 3 >> Attempt {i}");
-                try
-                {
-                    result = await client.GetGrain<ITaskGrain>("key").Execute();
-                    Console.WriteLine(result.Count);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.ToString());
-                }
-                await Task.Delay(TimeSpan.FromSeconds(5));
-            }
+            result = await client.GetGrain<ITaskGrain>("key").Execute();
+            Console.WriteLine(result.Count);
 
         }
 
--- TestGrains/Constants.cs
+++ TestGrains/Constants.cs
@@ -8,7 +8,7 @@ namespace TestGrains
     {
         public const string ClusterId = "TestOrleansCluster";
         public const string ServiceId = "TestOrleansService";
-        public const string SqlServerConnectionString = "Data Source=localhost;Initial Catalog=TestOrleansDb;uid=testorleans;pwd=testorleans";
+        public const string SqlServerConnectionString = "Data Source=localhost;Initial Catalog=TestOrleansDb;uid=testorleans;pwd=testorleans;ConnectRetryCount=10;ConnectRetryInterval=3;";
         public const string SqlServerInvariant = "System.Data.SqlClient";
         public const string StorageName = "dev";
         public const int SiloPort = 30000;
